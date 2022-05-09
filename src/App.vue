50ae7085daa6e070dbf433c92086961184343478
Pierre-Louis de laporte <pierre-louisdelaporte@MacBook-Air-de-Pierre-Louis.local>
2022-05-09 13:51:30 +0200

    Added my project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useSound } from '@vueuse/sound'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
